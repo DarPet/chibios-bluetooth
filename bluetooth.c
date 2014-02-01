82f14755b2a635ec455a6346e6358cbfece28c8e
DarPet <DarPet.exp@gmail.com>
2014-02-01 20:01:25 +0100

    new modular design start

---
+++ bluetooth.c
@@ -0,0 +1,42 @@
+#include "bluetooth.h"
+
+
+/*!
+ * \brief Sends a buffer of data through the specified BluetoothDriver
+ *
+ * \param[in] instance A BluetoothDriver object
+ * \param[in] command Any command for the recieving side
+ * \param[in] buffer A pointer to a buffer
+ * \param[in] bufferlength The length of the buffer
+ * \return EXIT_SUCCESS or EXIT_FAILURE
+ */
+
+int btSend(BluetoothDriver *instance, int command, char *buffer, int bufferlength){
+
+
+
+
+    return EXIT_SUCCESS;
+}
+
+/*!
+ *  \brief Check if there is an incoming frame to be processed
+ *
+ * \param[in] instance BluetoothDriver to use
+ * \return 0 if there is no frame, 1 if there is a frame
+ */
+
+int btIsFrame(BluetoothDriver *instance);
+
+/*!
+ * \brief Reads data from bluetooth module
+ *
+ * Copy the given (bufferlen sized) buffer to the specified BluetoothDriver input queue to send through the bluetooth device
+ *
+ * \param[in] instance A BluetoothDriver object
+ * \param[in] buffer A pointer to a buffer
+ * \param[in] maxlen The length of the buffer
+ * \return EXIT_SUCCESS or EXIT_FAILURE
+ */
+
+int btRead(BluetoothDriver *instance, char *buffer, int maxlen);
--- bluetooth.h
+++ bluetooth.h
@@ -1,6 +1,44 @@
+/*!
+ * @file bluetooth.h
+ * @brief Header file for bluetooth module in ChibiosRT.
+ *
+ * @addtogroup BLUETOOTH
+ * @{
+ */
+
+
 #ifndef BLUETOOTH_H_INCLUDED
 #define BLUETOOTH_H_INCLUDED
+
+#if HAL_USE_BLUETOOTH || defined(__DOXYGEN__)
+
+struct BluetoothDeviceVMT {
+    int (*sendBuffer)(void *instance, char *buffer, int bufferlength);
+    int (*sendCommandByte)(void *instance, int commandByte);
+    int (*canRecieve)(void *instance);
+    int (*readBuffer)(void *instance, char *buffer, int maxlength);
+    int (*setPinCode)(void *instance, char *pin, int pinlength);
+    int (*setName)(void *instance, char *newname, int namelength);
+};
+
+typedef struct BluetoothConfig BluetoothConfig;
+
+typedef struct BluetoothDriver{
+    const struct BluetoothDeviceVMT *vmt;
+
+};
+
+
+
+
+int btSend(BluetoothDriver *instance, int commandByte, char *buffer, int bufferlength);
+
+int btIsFrame(BluetoothDriver *instance);
+
+int btRead(BluetoothDriver *instance, char *buffer, int maxlen);
+
 
 
 
 #endif // BLUETOOTH_H_INCLUDED
+/** @} */
